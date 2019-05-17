d60b9d8532786a0102a01a918676a4de2ceef77a
KevSib <ksibue@gmail.com>
2019-05-17 16:29:27 +0200

    create unfollow handler

--- Mixter.Domain/Core/Subscriptions/Handlers/UpdateFollowers.cs
+++ Mixter.Domain/Core/Subscriptions/Handlers/UpdateFollowers.cs
@@ -3,7 +3,7 @@
 namespace Mixter.Domain.Core.Subscriptions.Handlers
 {
     [Handler]
-    public class UpdateFollowers: IEventHandler<UserFollowed>
+    public class UpdateFollowers: IEventHandler<UserFollowed>, IEventHandler<UserUnfollowed>
     {
         private readonly IFollowersRepository _repository;
             
@@ -20,6 +20,16 @@ namespace Mixter.Domain.Core.Subscriptions.Handlers
             }
                 
             _repository.Save(new FollowerProjection(evt.SubscriptionId.Followee, evt.SubscriptionId.Follower));
+        }
+        
+        public void Handle(UserUnfollowed evt)
+        {
+            if (_repository == null)
+            {
+                return;
+            }
+                
+            _repository.Remove(new FollowerProjection(evt.SubscriptionId.Followee, evt.SubscriptionId.Follower));
         }
     }
 }
